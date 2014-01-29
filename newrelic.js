83be979bf46a5f5c9bd3ce9fbf2f2912f6a52c3e
Paul Kenny <kenny.paul@gmail.com>
2014-01-29 16:33:53 +0000

    Adding new relic monitoring

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Daon Voice Demo'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '64070a7b5528ba963c87e281a1e761487feaeca5',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
